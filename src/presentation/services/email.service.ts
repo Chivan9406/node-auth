f37255179fa9b23fda67dbfdc3fafda4e6809f92
Chivan9406 <ivan9406@hotmail.com>
2024-02-11 18:27:25 -0600

    auth middleware and pagination

--- src/presentation/services/email.service.ts
+++ src/presentation/services/email.service.ts
@@ -19,6 +19,7 @@ export class EmailService {
         mailerService: string,
         mailerEmail: string,
         senderEmailPassword: string,
+        private readonly postToProvider: boolean,
     ) {
         this.transporter = nodemailer.createTransport({
             service: mailerService,
@@ -33,6 +34,8 @@ export class EmailService {
         const {to, subject, htmlBody, attachments = []} = options
 
         try {
+            if (!this.postToProvider) return true;
+
             const sentInformation = await this.transporter.sendMail({
                 to,
                 subject,
