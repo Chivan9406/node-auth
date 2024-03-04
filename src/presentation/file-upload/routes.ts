7f069c920ac677457d01e72bd05e3fc968dc702c
Chivan9406 <ivan9406@hotmail.com>
2024-03-03 19:21:30 -0600

    Fix multiple files

--- src/presentation/file-upload/routes.ts
+++ src/presentation/file-upload/routes.ts
@@ -18,4 +18,3 @@ export class FileUploadRoutes {
         return router
     }
 }
-
--- src/presentation/services/file-upload.service.ts
+++ src/presentation/services/file-upload.service.ts
@@ -6,7 +6,7 @@ import {CustomError} from '../../domain'
 
 export class FileUploadService {
     constructor(
-        private readonly uuid = Uuid.v4()
+        private readonly uuid = Uuid.v4
     ) {
     }
 
@@ -31,7 +31,7 @@ export class FileUploadService {
             const destination = path.resolve(__dirname, '../../../', folder)
             this.checkFolder(destination)
 
-            const fileName = `${this.uuid}.${fileExtension}`
+            const fileName = `${this.uuid()}.${fileExtension}`
             await file.mv(`${destination}/${fileName}`)
             return {fileName}
 
@@ -47,8 +47,8 @@ export class FileUploadService {
     ) {
         const fileNames = await Promise.all(
             files.map(file => this.uploadSingle(file, folder, validExtensions))
-        );
+        )
 
-        return fileNames;
+        return fileNames
     }
 }
